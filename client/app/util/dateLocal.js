328984215345d4a58b6cabbbc00fb9b3ac16581e
Mark <zzzmarkelzzz@gmail.com>
2017-02-25 12:37:39 +0200

    Deleted old files

--- client/app/util/dateLocal.js
+++
@@ -1,5 +0,0 @@
-let monthLocalization = [ "Января", "Февраля", "Марта", "Апреля", "Мая", "Июня", "Июля", "Августа", "Сентября", "Октября", "Ноября", "Декабря"];
-
-export default function localizeDate(monthNum) {
-    return monthLocalization[monthNum];
-}
